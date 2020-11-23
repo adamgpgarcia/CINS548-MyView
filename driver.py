152236259289c43d46b3f92ba77233e11bbfa246
Nathan Tisdale <natisdale@mail.csuchico.edu>
2020-11-23 15:27:25 -0800

    Load URLs from db and additional debug level logging

--- driver.py
+++ driver.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 # Author: Nathan Tisdale
-# Purpose: proof of concept: use selenium to drive dakboard rotation
+# Purpose: load urls and use selenium to drive dakboard rotation
+import os
 import time
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
@@ -11,20 +12,39 @@ from selenium.webdriver.common.by import By
 import logging
 import logging.handlers
 from logging.handlers import SysLogHandler
+import sqlite3
 
 # Instantiate logger: adapted from https://www.kite.com/python/docs/logging.handlers.SysLogHandler
 logger = logging.getLogger('mylogger')
-logger.setLevel(logging.ERROR)
-
+#logger.setLevel(logging.ERROR)
+logger.setLevel(logging.DEBUG)
 handler = logging.handlers.SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address='/dev/log')
 logger.addHandler(handler)
 
+#Get location of file
+dir_path = os.path.dirname(os.path.realpath(__file__))
+#Name of MyView database
+db_file = "db.sqlite3"
+# number of seconds to display a board
+displayTime = 5
 #boards list can be imported from file or db
-boards = [
-   "file:///opt/myview/credits.html",
-   "https://www.dakboard.com/app?p=26c8cd461ee43fe0c48bf4dcecd14ff2",
-   "https://tisdale.info/nothing/to/see/here"
-]
+boards = []
+conn = None
+cursor = None
+
+try:
+   conn = sqlite3.connect(dir_path + '/' + db_file)
+   #conn.execute("INSERT INTO view_viewuser (username, MacAdd, url, connect) VALUES ('Nate', 'b8:27:eb:b1:46:b8', 'https://www.dakboard.com/app?p=26c8cd461ee43fe0c48bf4dcecd14ff2', True)")
+   #conn.commit()
+   cursor = conn.execute("SELECT url FROM view_viewuser WHERE connect=true")
+   for row in cursor:
+      boards.append(row[0])
+      logger.debug("MyView: url added to rotation = " + row[0])
+except sqlite3.Error as error:
+   logger.error("MyView: " + error)
+finally:
+   boards.append("file:///" + dir_path + '/' + 'credits.html')
+
 
 #Set options for selenium chrome driver
 opt = Options()
@@ -51,6 +71,6 @@ while (True):
             boards.remove(url)
          finally:
             logger.debug("MyView loaded: " + url)
-      time.sleep(5)
+      time.sleep(displayTime)
 #tear down the driver
 driver.quit()
