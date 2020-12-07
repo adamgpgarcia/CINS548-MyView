84cff06f74455969ef6a03028b4526175a7c6a9d
Nathan Tisdale <natisdale@mail.csuchico.edu>
2020-12-07 14:44:41 -0800

    Added deltion of cookies to fix refresh bug and moved retreival of urls to getActiveList()

--- driver.py
+++ driver.py
@@ -16,27 +16,22 @@ from selenium.webdriver.common.by import By
 
 # number of seconds to display a board
 DISPLAY_TIME = 5
+# Instantiate logger
+# adapted from https://www.kite.com/python/docs/logging.handlers.SysLogHandler
+logger = logging.getLogger('mylogger')
+#logger.setLevel(logging.ERROR)
+logger.setLevel(logging.DEBUG)
+handler = logging.handlers.SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address='/dev/log')
+logger.addHandler(handler)
 
-def main():
-    ''' main function loads urls from sqlite3 db and uses selenium to drive dakboard rotation'''
-    # Instantiate logger
-    # adapted from https://www.kite.com/python/docs/logging.handlers.SysLogHandler
-    logger = logging.getLogger('mylogger')
-    #logger.setLevel(logging.ERROR)
-    logger.setLevel(logging.DEBUG)
-    handler = logging.handlers.SysLogHandler(facility=SysLogHandler.LOG_DAEMON, address='/dev/log')
-    logger.addHandler(handler)
-
-    #Get location of file
+def getActiveList():
     dir_path = os.path.dirname(os.path.realpath(__file__))
     #Name of MyView database
     db_file = "db.sqlite3"
     credits_page = "file:///" + dir_path + "/credits.html"
-    #boards list can be imported from file or db
     boards = []
     conn = None
     cursor = None
-
     try:
         conn = sqlite3.connect(dir_path + '/' + db_file)
         cursor = conn.execute("SELECT url FROM view_viewuser WHERE connect=true")
@@ -47,11 +42,14 @@ def main():
         logger.error("MyView: %s", error)
     finally:
         boards.append(credits_page)
+    return boards
 
-
+def main():
+    ''' main function loads urls from sqlite3 db and uses selenium to drive dakboard rotation'''
     #Set options for selenium chrome driver
     opt = Options()
-    opt.add_argument("--kiosk")
+    #opt.add_argument("--kiosk")
+    opt.add_argument("disk-cache-size=0")
     opt.add_experimental_option("useAutomationExtension", False)
     opt.add_experimental_option("excludeSwitches", ["enable-automation"])
     #formatter = SyslogBOMFormatter(logging.BASIC_FORMAT)
@@ -61,9 +59,12 @@ def main():
     timeout = 1
 
     while True:
+        boards = getActiveList()
         #display each url in boards
         for url in boards:
+            print(url)
             driver.get(url)
+            driver.delete_all_cookies()
             # test & remove bad links from rotation;
             # adapted from https://selenium-python.readthedocs.io/waits.html
             if driver.title != "MyView":
@@ -79,6 +80,8 @@ def main():
             time.sleep(DISPLAY_TIME)
     #tear down the driver
     driver.quit()
+
+
 
 if __name__ == "__main__":
     main()
